3c8db051f3afe53ed8e139a4c8f93534d1e045a2
Andrea Wu <andreaowu@berkeley.edu>
2013-10-06 15:52:11 -0700

    added text to opening window

--- gen/edu/berkeley/cs160/andreawu/prog3/R.java
+++ gen/edu/berkeley/cs160/andreawu/prog3/R.java
@@ -28,9 +28,12 @@ public final class R {
         public static final int view2=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080005;
+        public static final int nonstandard=0x7f080002;
         public static final int take=0x7f080001;
+        public static final int takepics=0x7f080003;
         public static final int view=0x7f080000;
+        public static final int viewpics=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -42,6 +45,9 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int nonstandard=0x7f050003;
+        public static final int takepics=0x7f050004;
+        public static final int viewpics=0x7f050005;
     }
     public static final class style {
         /** 
