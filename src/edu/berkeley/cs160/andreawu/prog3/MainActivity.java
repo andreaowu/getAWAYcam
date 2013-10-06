2bc890fe66f9f3d8238384a79909c095b5a63b48
Andrea Wu <andreaowu@berkeley.edu>
2013-10-06 15:13:56 -0700

    proper picture captions

--- src/edu/berkeley/cs160/andreawu/prog3/MainActivity.java
+++ src/edu/berkeley/cs160/andreawu/prog3/MainActivity.java
@@ -1,15 +1,23 @@
 package edu.berkeley.cs160.andreawu.prog3;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	private Button view;
+	private Button take;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		addListenerOnButton();
 	}
 
 	@Override
@@ -17,6 +25,32 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void addListenerOnButton() {
+		take = (Button) findViewById(R.id.take);
+		view = (Button) findViewById(R.id.view);
+		
+		take.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+		view.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
 	}
 
 }
