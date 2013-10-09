0b99ae10d0971d0cf5d5d39573ee7b725ff3d59c
Andrea Wu <andreaowu@berkeley.edu>
2013-10-08 21:54:00 -0700

    camera view showing

--- gen/edu/berkeley/cs160/andreawu/prog3/R.java
+++ gen/edu/berkeley/cs160/andreawu/prog3/R.java
@@ -28,12 +28,11 @@ public final class R {
         public static final int view2=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int nonstandard=0x7f080002;
+        public static final int about=0x7f080003;
+        public static final int action_settings=0x7f080004;
+        public static final int rl=0x7f080000;
         public static final int take=0x7f080001;
-        public static final int takepics=0x7f080003;
-        public static final int view=0x7f080000;
-        public static final int viewpics=0x7f080004;
+        public static final int view=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -42,6 +41,7 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int about=0x7f050006;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
--- src/edu/berkeley/cs160/andreawu/prog3/MainActivity.java
+++ src/edu/berkeley/cs160/andreawu/prog3/MainActivity.java
@@ -1,21 +1,58 @@
 package edu.berkeley.cs160.andreawu.prog3;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import android.view.View.OnTouchListener;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.widget.ImageButton;
+import android.hardware.Camera;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
-import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
-	private Button view;
-	private Button take;
-
+	private ImageButton view;
+	private ImageButton take;
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+	private Uri fileUri;
+	public static final int MEDIA_TYPE_IMAGE = 1;
+	public RelativeLayout layout;
+	private Camera cam;
+	private CameraPreview camPrev;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		layout = (RelativeLayout) findViewById(R.id.rl);
 		
 		addListenerOnButton();
 	}
@@ -26,17 +63,93 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) { // what happens when take picture
+	    if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+	        if (resultCode == RESULT_OK) {
+	            // Image captured and saved to fileUri specified in the Intent
+	            Toast.makeText(this, "Image saved to:\n" +
+	                     data.getData(), Toast.LENGTH_LONG).show();
+
+	        } else if (resultCode == RESULT_CANCELED) {
+	            // User cancelled the image capture
+	        } else {
+	            // Image capture failed, advise user
+	        }
+	    }
+		Location loc = getLocation();
+		sendHTTPRequest(loc.getLatitude(), loc.getLongitude());
+		
+	}
+	
+	private Location getLocation() {
+	    // Get the location manager
+	    LocationManager locationManager = (LocationManager) 
+	            getSystemService(LOCATION_SERVICE);
+	    Criteria criteria = new Criteria();
+	    String bestProvider = locationManager.getBestProvider(criteria, false);
+	    return locationManager.getLastKnownLocation(bestProvider);
+	}
+	
+	private void sendHTTPRequest(double lat, double lon) {
+		String url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=401cc508df900471ffd0d08438f2890c&accuracy=16&safe_search=1&has_geo=1&lat=" + lat + "37.87&lon=" + lon + "radius=20&radius_units=mi&per_page=1&format=json&nojsoncallback=1&auth_token=72157636297811496-634cad8b93b422a1&api_sig=ebd3d8c3623430b4bfeeb26061876d35";
+		HttpClient client = new DefaultHttpClient();
+		HttpResponse resp;
+		try {
+			resp = client.execute(new HttpGet(url));
+			StatusLine sl = resp.getStatusLine();
+			if (sl.getStatusCode() == HttpStatus.SC_OK) {
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				resp.getEntity().writeTo(out);
+				out.close();
+				String results = out.toString();
+				parseXML(results);
+			}
+			
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void parseXML(String results) {
+		try {
+			JSONObject jsn = new JSONObject(results);
+			int id = jsn.getJSONObject("photo").getInt("id");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 	
 	public void addListenerOnButton() {
-		take = (Button) findViewById(R.id.take);
-		view = (Button) findViewById(R.id.view);
+		take = (ImageButton) findViewById(R.id.take);
+		view = (ImageButton) findViewById(R.id.view);
 		
 		take.setOnClickListener(new OnClickListener() {
 
 			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
+			public void onClick(View v) {
 				
+				// Create a media file name
+			    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+			    File mediaFile = new File("IMG_" + timeStamp + ".jpg");
+			    try {
+			    	mediaFile.createNewFile();
+			    } catch (IOException e) {
+			    	Uri outputFileUri = Uri.fromFile(mediaFile);
+
+		            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); 
+		            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+		            startActivityForResult(cameraIntent, 0);
+			    }
 			}
 			
 		});
@@ -44,7 +157,7 @@ public class MainActivity extends Activity {
 		view.setOnClickListener(new OnClickListener() {
 
 			@Override
-			public void onClick(View arg0) {
+			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				
 			}
